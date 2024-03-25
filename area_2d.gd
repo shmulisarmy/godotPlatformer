40ec0142e5e90af97ae715f0be86c21b572d3832
shmuli <shmulikeller@gmail.com>
2024-03-25 19:28:10 -0400

    still working on player animation

---
+++ area_2d.gd
@@ -0,0 +1,12 @@
+extends Area2D
+@onready var game_manager = %game_manager
+
+
+# Called when the node enters the scene tree for the first time.
+
+
+
+func _on_body_entered(body):
+	if (body.name == "main_character"):
+		queue_free() # Replace with function body.
+		game_manager.add_points(10)
